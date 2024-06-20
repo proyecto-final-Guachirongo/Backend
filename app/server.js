2d2b06b72c5bb357c2512f063a498bdd6ef0860d
Samuel <samyvelezm@gmail.com>
2024-06-19 20:11:53 -0500

    Se agregan cors

--- app/server.js
+++ app/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import ruta from "./routes";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 app.use("/", ruta);
+app.use(cors())
 
 export default app;
